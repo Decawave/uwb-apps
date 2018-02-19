49fc05323fdd575d3bf4cb360056cc6e90048760
Paul Kettle <paul.kettle@gmail.com>
2018-02-19 14:35:17 -0800

    aoa example initial checkin

---
+++ apps/aoa_node/src/main.c
@@ -0,0 +1,229 @@
+/**
+ * Copyright (C) 2017-2018, Decawave Limited, All Rights Reserved
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <assert.h>
+#include <string.h>
+#include <stdio.h>
+#include "sysinit/sysinit.h"
+#include "os/os.h"
+#include "bsp/bsp.h"
+#include "hal/hal_gpio.h"
+#include "hal/hal_bsp.h"
+#ifdef ARCH_sim
+#include "mcu/mcu_sim.h"
+#endif
+
+#include <dw1000/dw1000_dev.h>
+#include <dw1000/dw1000_hal.h>
+#include <dw1000/dw1000_phy.h>
+#include <dw1000/dw1000_mac.h>
+#include <dw1000/dw1000_rng.h>
+#include <dw1000/dw1000_lwip.h>
+#include <dw1000/dw1000_ftypes.h>
+
+static dwt_config_t mac_config = {
+    .chan = 5,                          // Channel number. 
+    .prf = DWT_PRF_64M,                 // Pulse repetition frequency. 
+    .txPreambLength = DWT_PLEN_256,     // Preamble length. Used in TX only. 
+    .rxPAC = DWT_PAC8,                  // Preamble acquisition chunk size. Used in RX only. 
+    .txCode = 8,                        // TX preamble code. Used in TX only. 
+    .rxCode = 9,                        // RX preamble code. Used in RX only. 
+    .nsSFD = 0,                         // 0 to use standard SFD, 1 to use non-standard SFD. 
+    .dataRate = DWT_BR_6M8,             // Data rate. 
+    .phrMode = DWT_PHRMODE_STD,         // PHY header mode. 
+    .sfdTO = (512 + 1 + 8 - 8)              // SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. 
+};
+
+static dw1000_phy_txrf_config_t txrf_config = { 
+        .PGdly = TC_PGDELAY_CH5,
+        //.power = 0x25456585
+        .BOOSTNORM = dw1000_power_value(DW1000_txrf_config_9db, 5),
+        .BOOSTP500 = dw1000_power_value(DW1000_txrf_config_9db, 5),
+        .BOOSTP250 = dw1000_power_value(DW1000_txrf_config_9db, 5),
+        .BOOSTP125 = dw1000_power_value(DW1000_txrf_config_9db, 5)
+};
+
+static dw1000_rng_config_t rng_config = {
+    .tx_holdoff_delay = 0x4800,          // Send Time delay in usec.
+    .rx_timeout_period = 0xF000         // Receive response timeout in usec.
+};
+
+static twr_frame_t twr[] = {
+    [0].request = {
+        .fctrl = 0x8841,                // frame control (0x8841 to indicate a data frame using 16-bit addressing).
+        .PANID = 0xDECA,                 // PAN ID (0xDECA)
+        .code = DWT_TWR_INVALID
+    },
+    [1].request = {
+        .fctrl = 0x8841,                // frame control (0x8841 to indicate a data frame using 16-bit addressing).
+        .PANID = 0xDECA,                 // PAN ID (0xDECA)
+        .code = DWT_TWR_INVALID
+    }
+};
+
+
+// Calculates the time of flight in dwt-units x 1000 (for easy mm calc)
+int32_t twr_calc_tof_32(uint32_t tround1, uint32_t treply1, uint32_t tround2, uint32_t treply2)
+{
+    int64_t nom = ((int64_t)tround1*(int64_t)tround2 - (int64_t)treply1*(int64_t)treply2)*1000;
+    int64_t denom = (int64_t)tround1+(int64_t)tround2+(int64_t)treply1+(int64_t)treply2;
+    
+    return nom/denom;
+}
+
+
+void print_frame(const char * name, twr_frame_t * twr ){
+    printf("%s{\n\tfctrl:0x%04X,\n", name, twr->response.fctrl);
+    printf("\tseq_num:0x%02X,\n", twr->response.seq_num);
+    printf("\tPANID:0x%04X,\n", twr->response.PANID);
+    printf("\tdst_address:0x%04X,\n", twr->response.dst_address);
+    printf("\tsrc_address:0x%04X,\n", twr->response.src_address);
+    printf("\tcode:0x%04X,\n", twr->response.code);
+    printf("\treception_timestamp:0x%08lX,\n", twr->response.reception_timestamp); 
+    printf("\ttransmission_timestamp:0x%08lX,\n", twr->response.transmission_timestamp); 
+    printf("\trequest_timestamp:0x%08lX,\n", twr->request_timestamp); 
+    printf("\tresponse_timestamp:0x%08lX\n}\n", twr->response_timestamp); 
+    return;
+}
+
+
+/* The timer callout */
+static struct os_callout blinky_callout;
+
+/*
+ * Event callback function for timer events. It toggles the led pin.
+*/
+static void timer_ev_cb(struct os_event *ev) {
+    assert(ev != NULL);
+
+    hal_gpio_toggle(LED_BLINK_PIN);
+    dw1000_dev_instance_t * inst = hal_dw1000_inst(0);
+
+    dw1000_rng_request(inst, 0x4321, DWT_DS_TWR);
+
+    if (inst->status.start_rx_error)
+        printf("timer_ev_cb:[start_rx_error]\n");
+    if (inst->status.start_tx_error)
+        printf("timer_ev_cb:[start_tx_error]\n");
+    if (inst->status.rx_error)
+        printf("timer_ev_cb:[rx_error]\n");
+    if (inst->status.request_timeout)
+        printf("timer_ev_cb:[request_timeout]\n");
+    if (inst->status.rx_timeout_error)
+        printf("timer_ev_cb:[rx_timeout_error]\n");
+
+    if (inst->status.start_tx_error || inst->status.rx_error || inst->status.request_timeout ||  inst->status.rx_timeout_error){
+        inst->status.start_tx_error = inst->status.rx_error = inst->status.request_timeout = inst->status.rx_timeout_error = 0;
+    }
+        
+    else if (inst->rng->twr[0].response.code == DWT_SS_TWR_FINAL) {
+            twr_frame_t * twr  = &inst->rng->twr[0];    
+            print_frame("trw=",twr);
+
+            int32_t ToF = ((twr->response_timestamp - twr->request_timestamp) 
+                -  (twr->response.transmission_timestamp - twr->response.reception_timestamp))/2;
+
+            //float range = ToF * 299792458 * (1.0/499.2e6/128.0);
+            //printf("ToF = %lX, range = %f\n", ToF, range);
+
+            printf("ToF=%lX, res_req=%lX rec_tra=%lX\n", ToF, (twr->response_timestamp - twr->request_timestamp), (twr->response.transmission_timestamp - twr->response.reception_timestamp));
+    } else if (inst->rng->nframes > 1){
+                if (inst->rng->twr[1].response.code == DWT_DS_TWR_FINAL) {
+
+                    twr_frame_t * twr  = &inst->rng->twr[0];   
+                    print_frame("1st=", twr); 
+
+                    int32_t ToF = ((twr->response_timestamp - twr->request_timestamp) 
+                        -  (twr->response.transmission_timestamp - twr->response.reception_timestamp))/2;
+
+                    printf("ToF_1st=0x%08lX, res_req=0x%08lX rec_tra=0x%08lX\n", ToF, (twr->response_timestamp - twr->request_timestamp), (twr->response.transmission_timestamp - twr->response.reception_timestamp));
+
+                    twr  = &inst->rng->twr[1];    
+                    print_frame("2nd=", twr); 
+
+                    ToF = ((twr->response_timestamp - twr->request_timestamp) 
+                        -  (twr->response.transmission_timestamp - twr->response.reception_timestamp))/2;
+
+                    printf("ToF_2nd=0x%08lX, res_req=0x%08lX rec_tra=0x%08lX\n", ToF, (twr->response_timestamp - twr->request_timestamp), (twr->response.transmission_timestamp - twr->response.reception_timestamp));
+                    uint64_t T1R = (inst->rng->twr[0].response_timestamp - inst->rng->twr[0].request_timestamp); 
+                    uint64_t T1r = (inst->rng->twr[0].response.transmission_timestamp  - inst->rng->twr[0].response.reception_timestamp); 
+                    
+                    uint64_t T2R = (inst->rng->twr[1].response_timestamp - inst->rng->twr[1].request_timestamp); 
+                    uint64_t T2r = (inst->rng->twr[1].response.transmission_timestamp  - inst->rng->twr[1].response.reception_timestamp); 
+                    
+                    uint32_t Tprop  =  (T1R - T1r + T2R - T2r) >> 2;
+                    printf("Tprop=0x%08lX\n", Tprop);
+
+                    int32_t tof = twr_calc_tof_32(T1R&0xFFFFFFFF, T1r&0xFFFFFFFF, T2R&0xFFFFFFFF, T2r&0xFFFFFFFF);
+                    int32_t range_mm = (((int64_t)tof)<<4)/3411;
+                    printf("tof = %lX, range = %ld mm\n", tof, range_mm);
+    
+//                    json_rng_encode(inst->rng->ss_twr);
+//                    json_ftype_encode(&inst->rng->ss_twr[1].response);
+                }
+    }
+
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC/64);
+}
+
+/*
+* Initialize the callout for a timer event.
+*/
+static void init_timer(void) {
+    os_callout_init(&blinky_callout, os_eventq_dflt_get(), timer_ev_cb, NULL);
+    os_callout_reset(&blinky_callout, OS_TICKS_PER_SEC);
+}
+
+
+int main(int argc, char **argv){
+    int rc;
+    
+    sysinit();
+    hal_gpio_init_out(LED_BLINK_PIN, 1);
+    hal_gpio_init_out(LED_1, 1);
+    hal_gpio_init_out(LED_3, 1);
+    
+    dw1000_dev_instance_t * inst = hal_dw1000_inst(0);
+    dw1000_softreset(inst);
+    dw1000_phy_init(inst, &txrf_config);   
+    inst->PANID = 0xDECA;
+    inst->my_short_address = 0x1234;
+    dw1000_set_panid(inst,inst->PANID);
+    dw1000_mac_init(inst, &mac_config);
+    dw1000_rng_init(inst, &rng_config);
+    dw1000_rng_set_frames(inst, twr, sizeof(twr)/sizeof(twr_frame_t));
+    
+    printf("device_id=%lX\n",inst->device_id);
+    printf("PANID=%X\n",inst->PANID);
+    printf("DeviceID =%X\n",inst->my_short_address);
+    printf("partID =%lX\n",inst->partID);
+    printf("lotID =%lX\n",inst->lotID);
+    printf("xtal_trim =%X\n",inst->xtal_trim);
+
+    init_timer();
+
+    while (1) {
+        os_eventq_run(os_eventq_dflt_get());
+    }
+    assert(0);
+    return rc;
+}
+
