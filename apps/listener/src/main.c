cfc03f78bb8f8fa197a5b6c7435ee8613d9d4494
Niklas Casaril <niklas@casaril.com>
2018-11-22 16:03:11 +1000

    Added ability to read out CIR data to listener. Made this configurable through config api.

--- apps/listener/src/main.c
+++ apps/listener/src/main.c
@@ -22,8 +22,9 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#include "sysinit/sysinit.h"
-#include "os/os.h"
+#include <math.h>
+#include <os/mynewt.h>
+#include <config/config.h>
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
 #include "hal/hal_bsp.h"
@@ -43,10 +44,76 @@
 #include "dw1000/dw1000_phy.h"
 #include "dw1000/dw1000_mac.h"
 
+/* 
+ * Default Config 
+ */
+#define LSTNR_DEFAULT_CONFIG {        \
+        .acc_samples = MYNEWT_VAL(CIR_NUM_SAMPLES)  \
+    }
+
+static uint16_t acc_samples_to_load = 0;
+
+static char *lstnr_get(int argc, char **argv, char *val, int val_len_max);
+static int lstnr_set(int argc, char **argv, char *val);
+static int lstnr_commit(void);
+static int lstnr_export(void (*export_func)(char *name, char *val),
+  enum conf_export_tgt tgt);
+
+static struct lstnr_config_s {
+    char acc_samples[6];
+} lstnr_config = LSTNR_DEFAULT_CONFIG;
+
+static struct conf_handler lstnr_handler = {
+    .ch_name = "lstnr",
+    .ch_get = lstnr_get,
+    .ch_set = lstnr_set,
+    .ch_commit = lstnr_commit,
+    .ch_export = lstnr_export,
+};
+
+static char *
+lstnr_get(int argc, char **argv, char *val, int val_len_max)
+{
+    if (argc == 1) {
+        if (!strcmp(argv[0], "acc_samples"))  return lstnr_config.acc_samples;
+    }
+    return NULL;
+}
+
+static int
+lstnr_set(int argc, char **argv, char *val)
+{
+    if (argc == 1) {
+        if (!strcmp(argv[0], "acc_samples")) {
+            return CONF_VALUE_SET(val, CONF_STRING, lstnr_config.acc_samples);
+        } 
+    }
+    return OS_ENOENT;
+}
+
+static int
+lstnr_commit(void)
+{
+    conf_value_from_str(lstnr_config.acc_samples, CONF_INT16,
+                        (void*)&(acc_samples_to_load), 0);
+    return 0;
+}
+
+static int
+lstnr_export(void (*export_func)(char *name, char *val),
+             enum conf_export_tgt tgt)
+{
+    export_func("lstnr/acc_samples", lstnr_config.acc_samples);
+    return 0;
+}
+
+
 /* Incoming messages mempool and queue */
 struct uwb_msg_hdr {
     uint64_t ts;
     uint16_t dlen;
+    uint16_t acc_offset;
+    uint16_t acc_len;
     struct _dw1000_dev_rxdiag_t diag;
 };
 
@@ -63,6 +130,7 @@ static struct os_mbuf_pool g_mbuf_pool;
 static struct os_mempool g_mbuf_mempool;
 static os_membuf_t g_mbuf_buffer[MBUF_MEMPOOL_SIZE];
 static struct os_mqueue rxpkt_q;
+
 
 static void
 create_mbuf_pool(void)
@@ -85,6 +153,7 @@ process_rx_data_queue(struct os_event *ev)
     int rc;
     struct os_mbuf *om;
     struct uwb_msg_hdr *hdr;
+    struct _dw1000_cir_complex *cirp;
     int payload_len;
 
     hal_gpio_init_out(LED_BLINK_PIN, 0);
@@ -115,7 +184,22 @@ process_rx_data_queue(struct os_event *ev)
         {
             printf("%02x", print_buffer[i]);
         }
-        printf("\"}\n");
+        if (hdr->acc_offset > 0) {
+            cirp = (struct _dw1000_cir_complex *) (print_buffer + hdr->acc_offset);
+            float idx = (float)(hdr->diag.fp_idx >> 6) + (hdr->diag.fp_idx&0x3F)*(1.0/64);
+            printf("\",\"cir\":{\"fp_idx\":\"%d.%03d\",\"real\":", 
+                   (int)idx, (int)(1000*(idx-(int)idx)));
+            for (int i=0;i<hdr->acc_len;i++) {
+                printf("%c%d", (i==0)? '[':',', cirp[i].real);
+            }
+            printf("],\"imag\":");
+            for (int i=0;i<hdr->acc_len;i++) {
+                printf("%c%d", (i==0)? '[':',', cirp[i].imag);
+            }
+            printf("]}}\n");
+        } else {
+            printf("\"}\n");
+        }
     end_msg:
         os_mbuf_free_chain(om);
         memset(print_buffer, 0, sizeof(print_buffer));
@@ -152,18 +236,27 @@ rx_complete_cb(dw1000_dev_instance_t * inst, dw1000_mac_interface_t * cbs)
             assert(0);
         }
 
-        dw1000_read(inst, RX_TIME_ID, 2,
-                    (uint8_t*)&(hdr->diag.fp_amp),
-                    sizeof(uint16_t));
-        dw1000_read(inst, RX_FQUAL_ID, 2,
-                    (uint8_t*)&(hdr->diag.fp_amp2),
-                    sizeof(uint16_t)*3);
-        hdr->diag.pacc_cnt =
-            (dw1000_read_reg(inst, RX_FINFO_ID, 2, sizeof(uint16_t)) & 0xFFF0) >> 4;
+        dw1000_read_rxdiag(inst, &hdr->diag);
         
         rc = os_mbuf_copyinto(om, 0, buffer, hdr->dlen);
         if (rc != 0) {
             return true;
+        }
+        /* Do we need to load accumulator data */
+        if (acc_samples_to_load) {
+            float idx = (float)(hdr->diag.fp_idx >> 6) + (hdr->diag.fp_idx&0x3F)*(1.0/64);
+            uint16_t fp_idx = roundf(idx) - 4*sizeof(struct _dw1000_cir_complex);
+
+            hdr->acc_offset = hdr->dlen;
+            hdr->acc_len = acc_samples_to_load;
+            dw1000_read_accdata(inst, buffer, fp_idx * sizeof(struct _dw1000_cir_complex),
+                                hdr->acc_len * sizeof(struct _dw1000_cir_complex) + 1);
+
+            rc = os_mbuf_copyinto(om, hdr->acc_offset, buffer+1,
+                                  hdr->acc_len * sizeof(struct _dw1000_cir_complex));
+        } else {
+            hdr->acc_offset = 0;
+            hdr->acc_len = 0;
         }
         rc = os_mqueue_put(&rxpkt_q, os_eventq_dflt_get(), om);
         if (rc != 0) {
@@ -171,6 +264,9 @@ rx_complete_cb(dw1000_dev_instance_t * inst, dw1000_mac_interface_t * cbs)
         }
     } else {
         /* Not enough memory to handle incoming packet, drop it */
+    }
+    if (!inst->config.dblbuffon_enabled) {
+        dw1000_start_rx(inst);
     }
     return true;
 }
@@ -232,6 +328,7 @@ int main(int argc, char **argv){
     hal_gpio_init_out(LED_BLINK_PIN, 1);
 
     uwbcfg_register(&uwb_cb);
+    conf_register(&lstnr_handler);
     conf_load();
     
     dw1000_dev_instance_t * inst = hal_dw1000_inst(0);
